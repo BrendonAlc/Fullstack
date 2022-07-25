4e7eb61ac3f251eaf6ad4f8a2dd483a759abc6f4
Brendon Alcantara <92334562+BrendonAlc@users.noreply.github.com>
2022-07-25 17:33:46 -0300

    Add files via upload
    Noções em TypeScript e JavaScript

---
+++ Bootcamp_Fullstack/JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('Brendon', 26));
+console.log(mostraCidade('Brendon', 'Pinhais'));
+console.log(mostraHobby('Luciana', 'Jogar'));
